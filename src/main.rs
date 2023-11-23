c66913984ad363be10a2a393277b8ae3611f9fd0
Gurkan <hello@gurkz.me>
2023-11-23 17:08:07 +0100

    camera

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-use bevy::prelude::*;
+use bevy::{
+    log::{Level, LogPlugin},
+    prelude::*,
+};
+use plugins::camera::CameraPlugin;
+
+mod plugins;
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(LogPlugin {
+            level: Level::DEBUG,
+            filter:
+                "wgpu=error,naga=error,bevy_render=error,bevy_app=info,bevy_ecs=info".to_string(),
+        }))
+        .add_plugins(CameraPlugin)
+        .run();
 }
---
+++ src/plugins/camera.rs
@@ -0,0 +1,56 @@
+use bevy::prelude::*;
+
+pub struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup_camera)
+            .add_systems(Update, camera_movement);
+    }
+}
+
+// orthographic camera setup
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+pub fn camera_movement(
+    input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut OrthographicProjection), With<Camera>>,
+    time: Res<Time>,
+) {
+    if let Ok((mut transform, mut ortho)) = query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if input.pressed(KeyCode::W) {
+            direction.y += 1.0;
+        }
+
+        if input.pressed(KeyCode::A) {
+            direction.x -= 1.0;
+        }
+
+        if input.pressed(KeyCode::S) {
+            direction.y -= 1.0;
+        }
+
+        if input.pressed(KeyCode::D) {
+            direction.x += 1.0;
+        }
+
+        if input.pressed(KeyCode::X) {
+            ortho.scale -= 0.1;
+        }
+
+        if input.pressed(KeyCode::Z) {
+            ortho.scale += 0.1;
+        }
+
+        if ortho.scale < 0.5 {
+            ortho.scale = 0.5;
+        }
+
+        let translation = &mut transform.translation;
+        *translation += time.delta_seconds() * 500.0 * direction;
+    }
+}
